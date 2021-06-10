0c1098d9ebd501cc28e1751030884f2d0205fc66
Rian Girard <riangirard@gmail.com>
2021-06-10 12:06:49 -0700

    updated for validations

--- semi_restful_tv_app/models.py
+++ semi_restful_tv_app/models.py
@@ -1,4 +1,25 @@
 from django.db import models
+# import re
+
+class ShowManager(models.Manager):
+    def basic_validator(self, postData, source):
+        errors = {}
+        if len(postData['title']) < 1: 
+            errors['title'] = 'Title name missing'
+        all_shows = Show.objects.all()              # instantiate all_shows
+        for show in all_shows:                      # loop through all_shows
+            if postData['title'] == show.title and source == 'create':    # error if postData title == show.title | rule applies to source = 'create', not 'update'
+                errors['title'] = 'That show has already been entered!'
+        if len(postData['network']) < 1: 
+            errors['network'] = 'Select a network'
+        if len(postData['release_date']) < 1: 
+            errors['release_date'] = 'Select a release date'
+        if len(postData['description']) != 0 and len(postData['description']) < 10:
+            errors['description'] = 'Description not long enough'
+        # EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        # if not EMAIL_REGEX.match(postData['email']):
+        #     errors['email'] = ('Invalid email address!')
+        return errors
 
 class Show(models.Model):
     title = models.CharField(max_length=255)
@@ -7,6 +28,7 @@ class Show(models.Model):
     description = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = ShowManager()     # new line replaces default value of objects = models.Manager()
 
     def __repr__(self):
         return f"<Show object: {self.title} | {self.network} | ID#: {self.id}>"
--- semi_restful_tv_app/views.py
+++ semi_restful_tv_app/views.py
@@ -1,7 +1,8 @@
 from django.db.models.fields import DateField
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import Show
-from datetime import datetime
+# from datetime import datetime     # no longer needed, see comments in def update(): 
 
 def index(request):
     return redirect('/shows')
@@ -25,17 +26,25 @@ def edit(request, id):
     return render(request, "edit.html", context)
 
 def update(request, id):
-    if request.method == "POST":
+    errors = Show.objects.basic_validator(request.POST, 'update')
+    if len(errors) > 0: 
+        for value in errors.values():
+            messages.error(request, value)
+        print('yeas!')
+        return redirect(f'/shows/{id}/edit')
+    
+    elif request.method == "POST":
         instance = Show.objects.get(id=id)      # use 'id' from urls.py; could also embed a hidden field into html form to make work
         instance.title=request.POST['title']
         instance.network=request.POST['network']
-        # originally I had 'release_date' on form set up as text, not "date", which means the request.POST came through as string, not date object
-        # This meant I had to bring it into a variable and then convert that string variable into a date object as follows: 
-        # rawdate=request.POST['release_date']   # use variable 'rawdate' as the string version from POST request
-        # objdate=datetime.strptime(rawdate, '%Y-%m-%d') # use datetime.strptime() method to convert string version to object version of date
-        instance.release_date=request.POST['release_date'] # because by 'release_date' is not set up as "date" on form, I can just assign it directly
+            # originally I had 'release_date' on form set up as text, not "date", which means the request.POST came through as string, not date object
+            # This meant I had to bring it into a variable and then convert that string variable into a date object as follows: 
+            # rawdate=request.POST['release_date']   # use variable 'rawdate' as the string version from POST request
+            # objdate=datetime.strptime(rawdate, '%Y-%m-%d') # use datetime.strptime() method to convert string version to object version of date
+        instance.release_date=request.POST['release_date'] # because by 'release_date' is now set up as "date" on form, I can just assign it directly
         instance.description=request.POST['description']
         instance.save()
+        messages.success(request, "Show successfully updated")
     
     return redirect(f'/shows/{id}')
 
@@ -51,11 +60,21 @@ def new(request):
     return render(request, "new.html")
 
 def create(request):
-    if request.method == "POST":
-        Show.objects.create(title=request.POST['title'], network=request.POST['network'], release_date=request.POST['release_date'], description=request.POST['description'])
-    instance = Show.objects.get(title=request.POST['title'])
+    errors = Show.objects.basic_validator(request.POST, 'create')
+    if len(errors) > 0:
+        for value in errors.values():
+            messages.error(request, value)
 
-    return redirect(f'/shows/{instance.id}')
+        return redirect('/new')
+
+    elif request.method == "POST":
+        Show.objects.create(title=request.POST['title'], network=request.POST['network'], release_date=request.POST['release_date'], description=request.POST['description'])
+        instance = Show.objects.get(title=request.POST['title'])
+        messages.success(request, "Show successfully created")
+
+        return redirect(f'/shows/{instance.id}')
+    
+    return redirect('/shows')
 
 
 # Create your views here.
